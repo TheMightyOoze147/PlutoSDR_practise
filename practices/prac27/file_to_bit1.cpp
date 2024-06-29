7e9eee86e3d282064a91c78c1edc436e08ccd293
TheMightyOoze147 <danilyuk100103@gmail.com>
2024-06-30 06:39:36 +0700

    emptty commit

--- practices/prac27/file_to_bit1.cpp
+++ practices/prac27/file_to_bit1.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(){
     fstream file;
     int a = 0 ;
-    file.open("/home/ivan/Pictures/Screenshots/Screenshot from 2024-04-18 17-50-22.png", ios::in | ios::binary);
+    file.open("/home/TheMightyOoze147/Pictures/Screenshots/Screenshot from 2024-04-18 17-50-22.png", ios::in | ios::binary);
         
         if(file.is_open()){
         cout<<"[LOG] : File is ready to Transmit.\n";
